85d437dcc3c7568c39bc1021cb42622241819b91
JuanC-Cabrera <juan.c.cabrera.dev@gmail.com>
2025-02-23 15:54:03 -0600

    script eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
@@ -10,16 +11,4 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {
-    settings: {
-      react: {
-        version: "detect", // Detecta la versión de React automáticamente
-      },
-    },
-  },
-  {
-    rules: {
-      "react/react-in-jsx-scope": "off", // Deshabilita la regla que requiere importar `React`
-    },
-  },
 ];
